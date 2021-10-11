ddc84814d814cf60c548ae3b2f160a1638b8c4a7
ivgtr <ivgtr.me@gmail.com>
2021-10-11 19:09:19 +0900

    :sparkles: add: テストの追加などなど

--- src/__tests__/mdParser.test.ts
+++ src/__tests__/mdParser.test.ts
@@ -1,7 +1,39 @@
 import { markdownParser } from "../index";
 
-describe("markdownParser", () => {
-  test("should return an array of objects", () => {
-    markdownParser("before**test**after");
+describe("Parsed markdown text", () => {
+  // 見出し
+  test("Parsed h1 text", () => {
+    const h1Text = markdownParser("# h1text");
+
+    expect(h1Text).toEqual("<h1>h1text</h1>");
+  });
+  // 強調
+  test("Parsed strong text(Asterisk)", () => {
+    const strongText = markdownParser("before**test**after");
+
+    expect(strongText).toEqual("<p>before<strong>test</strong>after</p>");
+  });
+  test("Parsed strong text(UnderBar)", () => {
+    const strongText = markdownParser("before__test__after");
+
+    expect(strongText).toEqual("<p>before<strong>test</strong>after</p>");
+  });
+  // 斜体
+  test("Parsed Italic text(Asterisk)", () => {
+    const italicText = markdownParser("before*test*after");
+
+    expect(italicText).toEqual("<p>before<em>test</em>after</p>");
+  });
+  test("Parsed Italic text(UnderBar)", () => {
+    const italicText = markdownParser("before_test_after");
+
+    expect(italicText).toEqual("<p>before<em>test</em>after</p>");
+  });
+
+  // 打ち消し線
+  test("Parsed Strike text", () => {
+    const strikeText = markdownParser("before~~test~~after");
+
+    expect(strikeText).toEqual("<p>before<del>test</del>after</p>");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { parse } from "./parser/index";
 
+const generateHTML = (ast: any) => ast.toString();
+
 export const markdownParser = (markdown: string) => {
   const mdArray = markdown.split(/\r\n|\r|\n/);
   const mdNodeArray = mdArray.map((md) => parse(md));
-  return mdNodeArray;
+  const html = generateHTML(mdNodeArray);
+  return html;
 };
